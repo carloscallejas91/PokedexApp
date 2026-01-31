ff9ab79f934d52e80a6fa249fdc3baffc5ba2580
carloscallejas <carloscallejas91@outlook.com>
2026-01-31 10:40:32 -0800

    feat(network): adiciona NetworkClient utilizando URLSession

---
+++ Pokedex/Data/API/NetworkClient.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkClient.swift
+//  PokedexApp
+//
+//  Created by macOS on 31/01/26.
+//
+
+import Foundation
+
+final class NetworkClient: NetworkClientProtocol {
+    private let session: URLSession
+    private let decoder: JSONDecoder
+    
+    init(
+        session: URLSession = .shared,
+        decoder: JSONDecoder = JSONDecoder()
+    ) {
+        self.self.session = session
+        self.self.decoder = decoder
+    }
+    
+    func request<T: Decodable>(_ request: URLRequest) async throws -> T {
+        let (data, response) = try await session.data(for: request)
+        
+        if let http = response as? HTTPURLResponse,
+           !(200...299).contains(http.statusCode) {
+            throw URLError(.badServerResponse)
+        }
+        
+        return try decoder.decode(T.self, from: data)
+    }
+}

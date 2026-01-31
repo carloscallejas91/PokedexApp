333583a77d731cb2aa12fcc67b6eb7f64aaec613
carloscallejas <carloscallejas91@outlook.com>
2026-01-31 10:25:28 -0800

    feat(dto): adiciona PokemonListItemDTO

---
+++ Pokedex/Data/DTO/PokemonListItemDTO.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonListItemDTO.swift
+//  PokedexApp
+//
+//  Created by macOS on 31/01/26.
+//
+
+import Foundation
+
+struct PokemonListItemDTO: Decodable {
+    let name: String
+    let url: String
+}

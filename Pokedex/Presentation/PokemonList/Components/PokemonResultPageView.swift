8e925e99f7b44209f50fbf4079e2763c0a7c903b
carloscallejas <carloscallejas91@outlook.com>
2026-01-31 12:26:57 -0800

    feat(presentation): adicona PokemonResultPageView

---
+++ Pokedex/Presentation/PokemonList/Components/PokemonResultPageView.swift
@@ -0,0 +1,64 @@
+//
+//  PokemonResultPageView.swift
+//  PokedexApp
+//
+//  Created by macOS on 31/01/26.
+//
+
+import SwiftUI
+
+struct PokemonResultPageView: View {
+    let pokemons: [PokemonListViewData]
+    @Binding var isLoadingPage: Bool
+    let onAppearItem: (PokemonListViewData) -> Void
+    
+    private let columns = [
+        GridItem(.flexible(), spacing: 16),
+        GridItem(.flexible(), spacing: 16),
+    ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 16) {
+                ForEach(pokemons) { pokemon in
+                    NavigationLink {
+                        // PokemonDetailView(pokemonID: pokemon.id)
+                    } label: {
+                        PokemonPageItemView(pokemon: pokemon)
+                            .task {
+                                onAppearItem(pokemon)
+                            }
+                    }
+                }
+                
+                if isLoadingPage {
+                    HStack {
+                        Spacer()
+                        ProgressView()
+                        Spacer()
+                    }
+                    .padding()
+                }
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 8)
+        }
+        .background(.white)
+    }
+}
+
+#Preview {
+    struct PokemonResultListViewPreviewWrapper: View {
+        @State var isLoadingPage: Bool = false
+        
+        var body: some View {
+            PokemonResultPageView(
+                pokemons: PokemonListViewData.mockPokemonResult,
+                isLoadingPage: $isLoadingPage,
+                onAppearItem: { _ in }
+            )
+        }
+    }
+    
+    return PokemonResultListViewPreviewWrapper()
+}

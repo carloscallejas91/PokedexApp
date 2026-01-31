b815708d94d17838beedbbd7cc9b88dee9099a69
carloscallejas <carloscallejas91@outlook.com>
2026-01-31 09:06:54 -0800

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by macOS on 31/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
